d245d72224c41dd8c84e3a9940dfaef7bb50d757
Carla De Lira <carlamdelira@gmail.com>
2019-02-20 16:52:43 -0800

    Fixed Listing Object (1) error

--- listings/models.py
+++ listings/models.py
@@ -4,6 +4,7 @@ from realtors.models import Realtor
 
 class Listing(models.Model): 
     realtor = models.ForeignKey(Realtor,on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
@@ -25,8 +26,8 @@ class Listing(models.Model):
     is_published= models.BooleanField(default=True)
     list_date= models.DateTimeField(default=datetime.now,blank=True)
 
-def __str__(self): 
-    return self.title 
+    def __str__(self): 
+        return self.title
 
 
 
