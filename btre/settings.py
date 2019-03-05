e9b68c56cf3fd590827c89979fcc2f4ecd58870b
Carla De Lira <carlamdelira@gmail.com>
2019-03-04 23:04:54 -0800

    Created Dashboard capabilities.

--- btre/settings.py
+++ btre/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -142,3 +143,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+#Email config 
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER ='carlamdelira@gmail.com'
+EMAIL_HOST_PASSWORD='s3h6e9e3p6'
+EMAIL_USE_TLS = True
